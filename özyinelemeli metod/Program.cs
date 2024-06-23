8048c3aafc709fa45dd7446c91db596c792920d8
Meryem Çirkin <meryemcirkin18@gmail.com>
2024-06-24 01:57:15 +0300

    Bu C# programı, kullanıcının girdiği bir sayı (b) kadar olan 3'ün üslerini hesaplar ve ekrana yazdırır.

---
+++ özyinelemeli metod/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace özyinelemeli_metod
+{
+    class Program
+
+    {
+
+        static double j = 0, sonuc = 0;
+
+        static double fak(int a)
+
+        {
+
+            if (j <= a)
+
+            {
+
+                sonuc = Math.Pow(3, j);
+
+                j++;
+
+                Console.Write("{0} ", sonuc);
+
+                fak(a);
+
+            }
+
+            return sonuc;
+
+        }
+
+        static void Main(string[] args)
+
+        {
+
+            int b;
+
+            Console.Write("3 sayısının sıfırıncı üssünden kaçıncı üssüne kadar yazılsın = ");
+
+            b = int.Parse(Console.ReadLine());
+
+            fak(b);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
